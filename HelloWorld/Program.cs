42ae5214e031226cf945a3d36f73b36a5eb1259c
VAHC <victor10alha@gmail.com>
2023-09-30 22:10:21 -0300

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string helloworldMessage = "Hello World!!!";
+
+            Console.WriteLine(HelloWorld(helloworldMessage));
+        }
+
+        private static string HelloWorld(string message)
+        {
+            // Esta linea es para prueba 
+            return message + " Welcome to my console app";
+        }
+    }
+}
